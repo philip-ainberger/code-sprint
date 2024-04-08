16f4cef3904fe4bb70eafc564bdac53a9b4be3df
Philip <43697862+philip-ainberger@users.noreply.github.com>
2024-04-08 20:01:50 +0000

    Outsource repository to base

---
+++ src/backend/CodeSprint.Core/Repositories/IUserRefDataRepository.cs
@@ -0,0 +1,13 @@
+using CodeSprint.Core.Models;
+
+namespace CodeSprint.Core.Repositories;
+
+public interface IUserRefDataRepository<T> where T : BaseModel
+{
+    Task<T> AddAsync(Guid userId, T entity);
+    Task<T> UpdateAsync(Guid userId, T entity);
+    Task<bool> RemoveAsync(Guid userId, Guid id);
+    Task<T> GetByIdAsync(Guid userId, Guid id);
+    Task<ICollection<T>> GetAllAsync(Guid userId);
+    T GetById(Guid userId, Guid id);
+}
