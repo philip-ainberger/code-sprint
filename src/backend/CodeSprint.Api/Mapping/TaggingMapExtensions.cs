ebf0997113f78e479a0ca86336238cb455e1b591
Philip <43697862+philip-ainberger@users.noreply.github.com>
2024-04-14 20:22:46 +0000

    Start introducing mapping extensions

---
+++ src/backend/CodeSprint.Api/Mapping/TaggingMapExtensions.cs
@@ -0,0 +1,45 @@
+using Google.Protobuf.Collections;
+
+namespace CodeSprint.Api.Mapping;
+
+public static class TaggingMapExtensions
+{
+    public static Common.Grpc.Tagging.Tag ToProto(this Core.Models.Tag model)
+    {
+        return new Common.Grpc.Tagging.Tag
+        {
+            Id = model.Id.ToString(),
+            UserId = model.UserId.ToString(),
+            Name = model.Name
+        };
+    }
+
+    public static Core.Models.Tag ToEntity(this Common.Grpc.Tagging.CreateTagRequest proto, Guid userId)
+    {
+        return new Core.Models.Tag(
+            Guid.NewGuid(),
+            userId,
+            proto.Name
+        );
+    }
+    
+
+    public static Core.Models.Tag ToEntity(this Common.Grpc.Tagging.Tag proto)
+    {
+        return new Core.Models.Tag(
+            Guid.Parse(proto.Id),
+            Guid.Parse(proto.UserId),
+            proto.Name
+        );
+    }
+
+    public static Core.Models.Tag[] ToEntity(this RepeatedField<Common.Grpc.Tagging.Tag> proto)
+    {
+        return proto.Select(ToEntity).ToArray();
+    }
+
+    public static Guid[] ToEntityIds(this RepeatedField<string> proto)
+    {
+        return proto.Select(Guid.Parse).ToArray();
+    }
+}
