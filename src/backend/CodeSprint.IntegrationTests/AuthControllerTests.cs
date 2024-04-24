d6ecd32d07937fd3f4bd4660cef9ee2bb04b6f70
philip-ainberger <philip.ainberger@aon.at>
2024-04-24 09:33:05 +0200

    refactor integration tests

--- src/backend/CodeSprint.IntegrationTests/AuthControllerTests.cs
+++ src/backend/CodeSprint.IntegrationTests/AuthControllerTests.cs
@@ -10,175 +10,174 @@ namespace CodeSprint.IntegrationTests;
 
 public class AuthControllerTests : BaseApplicationTest
 {
+    private readonly HttpClient _httpClient;
+    private readonly IMongoCollection<User> _userCollection;
+    private readonly IMongoCollection<RefreshToken> _refreshTokenCollection;
+
     public AuthControllerTests(ApiWebApplicationFactory<Program> factory) : base(factory)
     {
+        _httpClient = factory.CreateDefaultClient();
+        _userCollection = _mongoCollectionProvider.GetCollection<User>("users");
+        _refreshTokenCollection = _mongoCollectionProvider.GetCollection<RefreshToken>("refresh_tokens");
     }
 
     [Fact]
     public async Task AuthCallback_SuccessfulOAuthLogin_RefreshTokenInCookie()
     {
-        // Arrange
-        var userCollection = _mongoCollectionProvider.GetCollection<User>("users");
-        var refreshTokenCollection = _mongoCollectionProvider.GetCollection<RefreshToken>("refresh_tokens");
-        var httpClient = _factory.CreateDefaultClient();
-
         // Act
-        var loginResult = await httpClient.GetAsync("/api/auth/callback?code=test");
-
+        var loginResult = await _httpClient.GetAsync("/api/auth/callback?code=test");
+        
         // Assert
-        loginResult.StatusCode.Should().Be(HttpStatusCode.Found);
-        
-        // should contain two header values. first for deletion and second for setting the cookie
-        loginResult.Headers.Should().Contain(c => c.Key == "Set-Cookie" && c.Value.Count() == 2);
-        var rawHeaderValue = loginResult.Headers.First(c => c.Key == "Set-Cookie").Value.Last();
-        var jwt = ExtractJwtFromCookie(rawHeaderValue);
-
-        jwt.Should().NotBeNullOrEmpty();
-        IsJwt(jwt).Should().BeTrue();
-
-        var userCount = userCollection.AsQueryable().Count();
-        userCount.Should().Be(1);
-        
-        var refreshTokenCount = refreshTokenCollection.AsQueryable().Count();
-        refreshTokenCount.Should().Be(1);
+        AssertHttpStatus(loginResult, HttpStatusCode.Found);
+        AssertHeaderForCookie(loginResult);
+        AssertSingleUserAndToken();
     }
 
     [Fact]
     public async Task AuthCallback_OverrideExistingRefreshToken_TokenOverriden()
     {
         // Arrange
-        var userCollection = _mongoCollectionProvider.GetCollection<User>("users");
-        var refreshTokenCollection = _mongoCollectionProvider.GetCollection<RefreshToken>("refresh_tokens");
-        var httpClient = _factory.CreateDefaultClient();
-
+        await _httpClient.GetAsync("/api/auth/callback?code=test");
+        var previousUpdated = _refreshTokenCollection.AsQueryable().First().UpdatedAt;
+        
         // Act
-        await httpClient.GetAsync("/api/auth/callback?code=test");
-        var previousUpdated = refreshTokenCollection.AsQueryable().First().UpdatedAt;
+        var loginResult = await _httpClient.GetAsync("/api/auth/callback?code=test");
 
-        var loginResult = await httpClient.GetAsync("/api/auth/callback?code=test");
-
-        // Assert
-        loginResult.StatusCode.Should().Be(HttpStatusCode.Found);
-
-        var userCount = userCollection.AsQueryable().Count();
-        userCount.Should().Be(1);
-
-        var refreshTokens = refreshTokenCollection.AsQueryable().ToList();
-        refreshTokens.Should().HaveCount(1);
-        refreshTokens[0].UpdatedAt.Should().BeAfter(previousUpdated);
-
-        var rawHeaderValue = loginResult.Headers.First(c => c.Key == "Set-Cookie").Value.Last();
-        var jwt = ExtractJwtFromCookie(rawHeaderValue);
-        refreshTokens[0].Token.Should().Be(jwt);
+        // Arrange
+        AssertHttpStatus(loginResult, HttpStatusCode.Found);
+        AssertTokenRefreshed(previousUpdated);
     }
 
     [Fact]
     public async Task AuthCallback_InvalidCallbackCode_BadRequest()
     {
-        // Act
-        var httpClient = _factory.CreateDefaultClient();
-        var loginResult = await httpClient.GetAsync("/api/auth/callback");
-
-        // Assert
-        loginResult.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        AssertHttpStatus(await _httpClient.GetAsync("/api/auth/callback"), HttpStatusCode.BadRequest);
     }
 
     [Fact]
     public async Task RefreshTokenAsync_InvalidRefreshToken_ReturnsUnauthorized()
     {
         // Arrange
-        var httpClient = _factory.CreateDefaultClient();
-        var invalidRefreshToken = "invalidRefreshToken";
-        httpClient.DefaultRequestHeaders.Add("Cookie", $"jwt_token={invalidRefreshToken}");
+        AddCookieToHttpClient("invalidRefreshToken");
 
         // Act
-        var response = await httpClient.GetAsync("/api/auth/refresh");
-
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        AssertHttpStatus(await _httpClient.GetAsync("/api/auth/refresh"), HttpStatusCode.Unauthorized);
     }
 
     [Fact]
     public async Task RefreshTokenAsync_MissingRefreshToken_ReturnsUnauthorized()
     {
-        // Arrange
-        var httpClient = _factory.CreateDefaultClient();
-
-        // Act
-        var response = await httpClient.GetAsync("/api/auth/refresh");
-
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        AssertHttpStatus(await _httpClient.GetAsync("/api/auth/refresh"), HttpStatusCode.Unauthorized);
     }
 
     [Fact]
     public async Task RefreshTokenAsync_OverrideExistingRefreshToken_ReturnAccessToken()
     {
         // Arrange
-        var refreshTokenCollection = _mongoCollectionProvider.GetCollection<RefreshToken>("refresh_tokens");
-        var httpClient = _factory.CreateDefaultClient();
-
-        await httpClient.GetAsync("/api/auth/callback?code=test");
-        var refreshToken = refreshTokenCollection.AsQueryable().First().Token;
-
-        httpClient = _factory.CreateDefaultClient();
-        httpClient.DefaultRequestHeaders.Add("Cookie", $"jwt_token={refreshToken}");
+        await _httpClient.GetAsync("/api/auth/callback?code=test");
+        var refreshToken = _refreshTokenCollection.AsQueryable().First().Token;
+        AddCookieToHttpClient(refreshToken);
 
         // Act
-        var response = await httpClient.GetAsync("/api/auth/refresh");
+        var response = await _httpClient.GetAsync("/api/auth/refresh");
 
         // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.OK);
-        var body = await response.Content.ReadFromJsonAsync<JwtDto>();
-        body.Should().NotBeNull();
-        body!.Token.Should().NotBeNullOrEmpty();
-        body!.ExpiresIn.Should().BeGreaterThan(1);
-
-        var rawHeaderValue = response.Headers.First(c => c.Key == "Set-Cookie").Value.Last();
-        var jwt = ExtractJwtFromCookie(rawHeaderValue);
-
-        jwt.Should().NotBeSameAs(refreshToken);
-        refreshTokenCollection.AsQueryable().First().Token.Should().NotBeSameAs(refreshToken, "Should be a new refresh token");
+        AssertHttpStatus(response, HttpStatusCode.OK);
+        AssertRefreshTokenUpdated(response, refreshToken);
     }
 
     [Fact]
     public async Task ValidateTokenAsync_MissingToken_ReturnsUnauthorized()
     {
-        // Arrange
-        var httpClient = _factory.CreateDefaultClient();
-
-        // Act
-        var response = await httpClient.GetAsync("/api/auth/validate");
-
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        AssertHttpStatus(await _httpClient.GetAsync("/api/auth/validate"), HttpStatusCode.Unauthorized);
     }
-    
-    
+
     [Fact]
     public async Task ValidateTokenAsync_UseRefreshTokenAsAccess_ReturnsUnauthorized()
     {
         // Arrange
-        var refreshTokenCollection = _mongoCollectionProvider.GetCollection<RefreshToken>("refresh_tokens");
-        var httpClient = _factory.CreateDefaultClient();
+        await _httpClient.GetAsync("/api/auth/callback?code=test");
+        var refreshToken = _refreshTokenCollection.AsQueryable().First().Token;
+        AddAuthorizationHeader(refreshToken);
 
-        await httpClient.GetAsync("/api/auth/callback?code=test");
-        var refreshToken = refreshTokenCollection.AsQueryable().First().Token;
+        // Act & Assert
+        AssertHttpStatus(await _httpClient.GetAsync("/api/auth/validate"), HttpStatusCode.Unauthorized);
+    }
 
-        httpClient = _factory.CreateDefaultClient();
-        httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + refreshToken);
+    [Fact]
+    public async Task ValidateTokenAsync_UseAccessToken_ReturnsSameValidToken()
+    {
+        // Arrange
+        await _httpClient.GetAsync("/api/auth/callback?code=test");
+        var refreshToken = _refreshTokenCollection.AsQueryable().First().Token;
+        AddCookieToHttpClient(refreshToken);
 
-        // Act
-        var response = await httpClient.GetAsync("/api/auth/validate");
+        var tokenResponse = await _httpClient.GetFromJsonAsync<JwtDto>("/api/auth/refresh");
+        AddAuthorizationHeader(tokenResponse!.Token);
 
-        // Assert
-        response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        // Act & Assert
+        var response = await _httpClient.GetAsync("/api/auth/validate");
+
+        response.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        var body = await response.Content.ReadFromJsonAsync<JwtDto>();
+        body!.Token.Should().Be(tokenResponse.Token);
+        body!.ExpiresIn.Should().BeLessThan(tokenResponse.ExpiresIn);
+    }
+
+    private void AssertHttpStatus(HttpResponseMessage response, HttpStatusCode expectedStatus)
+    {
+        response.StatusCode.Should().Be(expectedStatus);
+    }
+
+    private void AssertHeaderForCookie(HttpResponseMessage response)
+    {
+        response.Headers.Should().Contain(c => c.Key == "Set-Cookie" && c.Value.Count() == 2);
+        var jwt = ExtractJwtFromCookie(response.Headers.First(c => c.Key == "Set-Cookie").Value.Last());
+        jwt.Should().NotBeNullOrEmpty();
+        IsJwt(jwt).Should().BeTrue();
+    }
+
+    private void AssertSingleUserAndToken()
+    {
+        _userCollection.AsQueryable().Count().Should().Be(1);
+        _refreshTokenCollection.AsQueryable().Count().Should().Be(1);
+    }
+
+    private void AssertTokenRefreshed(DateTime previousUpdated)
+    {
+        var refreshTokens = _refreshTokenCollection.AsQueryable().ToList();
+        refreshTokens.Should().HaveCount(1);
+        refreshTokens[0].UpdatedAt.Should().BeAfter(previousUpdated);
+    }
+
+    private void AssertRefreshTokenUpdated(HttpResponseMessage response, string previousToken)
+    {
+        var body = response.Content.ReadFromJsonAsync<JwtDto>().Result;
+        body.Should().NotBeNull();
+        body!.Token.Should().NotBeNullOrEmpty();
+        body!.ExpiresIn.Should().BeGreaterThan(0);
+
+        var newToken = ExtractJwtFromCookie(response.Headers.First(c => c.Key == "Set-Cookie").Value.Last());
+        newToken.Should().NotBeSameAs(previousToken);
+
+        _refreshTokenCollection.AsQueryable().First().Token.Should().NotBeSameAs(previousToken, "Should be a new refresh token");
+    }
+
+    private void AddCookieToHttpClient(string token)
+    {
+        _httpClient.DefaultRequestHeaders.Add("Cookie", $"jwt_token={token}");
+    }
+
+    private void AddAuthorizationHeader(string token)
+    {
+        _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
     }
 
     private string ExtractJwtFromCookie(string cookie)
     {
         var cookieParts = cookie.Split(';').FirstOrDefault()?.Split('=');
-        return cookieParts.Length > 1 ? cookieParts[1] : string.Empty;
+        return cookieParts!.Length > 1 ? cookieParts[1] : string.Empty;
     }
 
     private bool IsJwt(string token)
