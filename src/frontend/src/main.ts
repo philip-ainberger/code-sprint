a57fa583f43927162f653a014ac8aa1f261fe781
philip-ainberger <philip.ainberger@aon.at>
2024-04-28 10:40:08 +0200

    deploy to vercel

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core'; 
+import { environment } from './app/environments/environment';
+
+if (environment.production) { enableProdMode(); }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
